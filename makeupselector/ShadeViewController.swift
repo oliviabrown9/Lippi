65881eb187a0516fadb490450f060c83653c9537
Olivia Brown <obrown917@gmail.com>
2016-08-08 11:19:28 -0700

    Bug Fixes & Updating Comments

--- makeupselector/ShadeViewController.swift
+++ makeupselector/ShadeViewController.swift
@@ -2,50 +2,33 @@ import UIKit
 
 class ShadeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
 {
-    var button_number:Int = Int()
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let numberOfCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "aa", "bb"]
+    //        let numberOfCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "aa", "bb", "cc"]
     
-    var detaildata: DetailData = DetailData()
+    let imageArray = [UIImage(named: "biteAmuseBoucheCayenneLipstick"), UIImage(named: "biteAmuseBouchePersimmonLipstick"), UIImage(named: "katVonDStuddedKissArchangelLipstick"), UIImage(named: "katVonDStuddedKissBacheloretteLipstick"), UIImage(named: "katVonDStuddedKissHellbentLipstick"), UIImage(named: "katVonDStuddedKissHexagramLipstick-1"), UIImage(named: "katVonDStuddedKissUnderageRedLipstick"), UIImage(named: "narsFireDownBelowLipstick"), UIImage(named: "narsJungleRedLipstick"), UIImage(named: "narsRedLizardLipstick"), UIImage(named: "narsShanghaiExpressLipstick"), UIImage(named: "sephoraRougeMatteWantedLipstick"), UIImage(named: "urbanDecayViceBadBloodLipstick"), UIImage(named: "urbanDecayViceHexLipstick"), UIImage(named: "urbanDecayViceSheerFBombLipstick"), UIImage(named: "urbanDecayViceZealotLipstick"), UIImage(named: "givenchyLeRougeCarminEscarpinLipstick"), UIImage(named: "givenchyLeRougeRougeEgerieLipstick"), UIImage(named: "givenchyRougeInterditVinylRougeRebelleLipstick"), UIImage(named: "louboutinVelvetMatteDivaLipstick"), UIImage(named: "narsAudaciousRitaLipstick"), UIImage(named: "narsAudaciousAnnabellaLipstick"), UIImage(named: "smashboxBeLegendaryBingLipstick"), UIImage(named: "smashboxBeLegendaryUnzippedLipstick"), UIImage(named: "makeUpForEverRougeArtistRed"), UIImage(named: "makeUpForEverRougeArtistRedBrick"), UIImage(named: "smashboxBeLegendaryMadeItLipstick"), UIImage(named: "bobbiBrownLipRedLipstick")]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return self.numberOfCases.count
+        return self.imageArray.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
             as! CollectionViewCell
-        if self.button_number == 1 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray[indexPath.row])
-        }else if self.button_number == 2 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray_pink[indexPath.row])
-        }else if self.button_number == 3 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray_berry[indexPath.row])
-        }else if self.button_number == 4 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray_coral[indexPath.row])
-        }else if self.button_number == 5 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray_nude[indexPath.row])
-        }else if self.button_number == 6 {
-            cell.imageView?.image = UIImage(named: detaildata.imageArray_anything[indexPath.row])
-        }
         
-        cell.titleLabel?.text = self.numberOfCases[indexPath.row]
+        cell.imageView?.image = self.imageArray[indexPath.row]
         
         return cell
         
@@ -64,86 +47,11 @@ class ShadeViewController: UIViewController, UICollectionViewDelegate, UICollect
             
             let vc = segue.destinationViewController as! SpecificShadeViewController
             
-            if self.button_number == 1 {
-                vc.image = UIImage(named: detaildata.imageArray[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array[indexPath.row]
-                vc.title2_value = detaildata.title2_array[indexPath.row]
-                vc.title3_value = detaildata.title3_array[indexPath.row]
-                vc.price_value = detaildata.price_array[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array[indexPath.row]
-            }else if self.button_number == 2 {
-                vc.image = UIImage(named: detaildata.imageArray_pink[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array_pink[indexPath.row]
-                vc.title2_value = detaildata.title2_array_pink[indexPath.row]
-                vc.title3_value = detaildata.title3_array_pink[indexPath.row]
-                vc.price_value = detaildata.price_array_pink[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array_pink[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array_pink[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array_pink[indexPath.row]
-            }else if self.button_number == 3 {
-                vc.image = UIImage(named: detaildata.imageArray_berry[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array_berry[indexPath.row]
-                vc.title2_value = detaildata.title2_array_berry[indexPath.row]
-                vc.title3_value = detaildata.title3_array_berry[indexPath.row]
-                vc.price_value = detaildata.price_array_berry[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array_berry[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array_berry[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array_berry[indexPath.row]
-            }else if self.button_number == 4 {
-                vc.image = UIImage(named: detaildata.imageArray_coral[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array_coral[indexPath.row]
-                vc.title2_value = detaildata.title2_array_coral[indexPath.row]
-                vc.title3_value = detaildata.title3_array_coral[indexPath.row]
-                vc.price_value = detaildata.price_array_coral[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array_coral[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array_coral[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array_coral[indexPath.row]
-            }else if self.button_number == 5 {
-                vc.image = UIImage(named: detaildata.imageArray_nude[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array_nude[indexPath.row]
-                vc.title2_value = detaildata.title2_array_nude[indexPath.row]
-                vc.title3_value = detaildata.title3_array_nude[indexPath.row]
-                vc.price_value = detaildata.price_array_nude[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array_nude[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array_nude[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array_nude[indexPath.row]
-            }else if self.button_number == 6 {
-                vc.image = UIImage(named: detaildata.imageArray_anything[indexPath.row])!
-                vc.title = self.numberOfCases[indexPath.row]
-                vc.title1_value = detaildata.title1_array_anything[indexPath.row]
-                vc.title2_value = detaildata.title2_array_anything[indexPath.row]
-                vc.title3_value = detaildata.title3_array_anything[indexPath.row]
-                vc.price_value = detaildata.price_array_anything[indexPath.row]
-                vc.whatitis_value = detaildata.whatitis_array_anything[indexPath.row]
-                vc.whatitdoes_value = detaildata.whatitdoes_array_anything[indexPath.row]
-                vc.weblink_values = detaildata.weblink_array_anything[indexPath.row]
-            }
-            
+            vc.image = self.imageArray[indexPath.row]!
         }
     }
+    @IBAction func unwindToShadeViewController(segue: UIStoryboardSegue) {
+        // defining method
+    }
     
-    
-}
-            if segue.identifier == "showImage"
-            {
-                let indexPaths = self.collectionView!.indexPathsForSelectedItems()!
-                let indexPath = indexPaths[0] as NSIndexPath
-                
-                let vc = segue.destinationViewController as! SpecificShadeViewController
-                
-                vc.image = self.imageArray[indexPath.row]!
-//                vc.title = self.numberOfCases[indexPath.row]
-            }
-        }
-        @IBAction func unwindToShadeViewController(segue: UIStoryboardSegue) {
-            // defining method
-        }
-        
 }
