fbbef2dc98a4076100cc05c1e076887305ef99d4
Olivia Brown <obrown917@gmail.com>
2016-08-28 13:22:06 -0700

    Switched textField to label to fix spacing

--- makeupselector/SpecificShadeViewController.swift
+++ makeupselector/SpecificShadeViewController.swift
@@ -25,7 +25,8 @@ class SpecificShadeViewController: UIViewController {
     @IBOutlet weak var buyNowButton: UIButton!
     @IBOutlet weak var title1: UITextView!
     @IBOutlet weak var title2: UITextView!
-    @IBOutlet weak var price: UITextView!
+//    @IBOutlet weak var price: UITextView!
+    @IBOutlet weak var price: UILabel!
     @IBOutlet weak var whatitis: UITextView!
     @IBOutlet weak var imageView2: UIImageView!
     @IBOutlet weak var shadeView: UIView!
@@ -34,23 +35,34 @@ class SpecificShadeViewController: UIViewController {
     @IBAction func buyNowButton(sender: AnyObject) {
         UIApplication.sharedApplication().openURL(webLink)
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        //Sets navigationBar tint color to pink
         navigationController?.navigationBar.tintColor = UIColor(red:0.98, green:0.42, blue:0.42, alpha:1.0)
+        
+        //Disables whatitis textfield scrolling - fixes error of poorly formatted text
         whatitis.scrollEnabled = false
+        
+        //idk man
+        price.adjustsFontSizeToFitWidth = true
+        price.minimumScaleFactor = 0.5
+
     }
     
     override func viewDidAppear(animated: Bool) {
+        //Enables whatitis textfield scrolling - fixes error of poorly formatted text
         whatitis.scrollEnabled = true
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        //Adds a cornerRadius to the shadeView and buyNowButton
         shadeView.layer.cornerRadius = 10
         buyNowButton.layer.cornerRadius = 15
         
+        //Passes correct variable for each specificShadeVC variable
 //        self.imageView.image = self.image
         self.imageView2.image = self.image
         webLink = NSURL(string: self.linkValue)
@@ -59,6 +71,4 @@ class SpecificShadeViewController: UIViewController {
         self.price.text = "$\(self.priceValue)"
         self.whatitis.text = self.whatItIsValue
     }
-    
-    
 }
