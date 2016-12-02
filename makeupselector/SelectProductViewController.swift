dfc1e9d1159889c2ec0780f30d6fd0eed6d78137
Olivia Brown <obrown917@gmail.com>
2016-12-02 01:08:38 -0800

    Updated Lipstick Button

--- makeupselector/SelectProductViewController.swift
+++ makeupselector/SelectProductViewController.swift
@@ -17,6 +17,12 @@ class SelectProductViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         lipstick.layer.cornerRadius = 15
+        lipstick.backgroundColor = UIColor(red:1.00, green:0.07, blue:0.14, alpha:1.0)
+//        lipstick.contentVerticalAlignment = .bottom
+        let imageEdgeInsets = UIEdgeInsets(top: -50, left: 50, bottom: 0, right: 0)
+        let titleEdgeInsets = UIEdgeInsets(top: 120, left: -115, bottom: 0, right: 0)
+        lipstick.imageEdgeInsets = imageEdgeInsets
+        lipstick.titleEdgeInsets = titleEdgeInsets
         liquidLipstick.layer.cornerRadius = 15
         
     }
