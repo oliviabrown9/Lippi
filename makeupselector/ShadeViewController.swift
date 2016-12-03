47356a347a47f1476092f7860c346906ab64a5a5
Olivia Brown <obrown917@gmail.com>
2016-12-02 21:51:27 -0800

    Shuffle Products

--- makeupselector/ShadeViewController.swift
+++ makeupselector/ShadeViewController.swift
@@ -46,6 +46,7 @@ class ShadeViewController: UIViewController, UICollectionViewDelegate, UICollect
     override func viewDidLoad() {
         super.viewDidLoad()
         self.getData()
+        temp_detaildata.shuffle()
         self.automaticallyAdjustsScrollViewInsets = false
         
         let minValue = priceArray.min()
@@ -178,4 +179,18 @@ extension UIImageView {
     }
 }
 
+extension MutableCollection where Indices.Iterator.Element == Index {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
+        }
+    }
+}
 
