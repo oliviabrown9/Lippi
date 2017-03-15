c2482e974ade113787d5d246ea82dbdb457af7de
Olivia <obrown917@gmail.com>
2017-03-14 20:45:47 -0700

    Updated storyboard for first two screens

--- makeupselector/SelectProductViewController.swift
+++ makeupselector/SelectProductViewController.swift
@@ -14,15 +14,9 @@ class SelectProductViewController: UIViewController {
     
     @IBOutlet weak var lipstick: UIButton!
     @IBOutlet weak var liquidLipstick: UIButton!
-    @IBOutlet weak var lipstickView: UIView!
-    @IBOutlet weak var liquidLipstickView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        lipstickView.layer.cornerRadius = 15
-        liquidLipstickView.layer.cornerRadius = 15
     }
-    
-    let messageComposer = MessageComposer()
     
     // Defining Product Button Values
     @IBAction func lipstickButtonClicked(_ sender: AnyObject) {
@@ -32,17 +26,5 @@ class SelectProductViewController: UIViewController {
     @IBAction func liquidLipstickButtonClicked(_ sender: AnyObject) {
         productButton = 2
         performSegue(withIdentifier: "toSelectShade", sender: sender)
-    }
-    @IBAction func contactUsButtonClicked(_ sender: UIButton) {
-        if (messageComposer.canSendText()) {
-            
-            // Obtain a configured MFMessageComposeViewController
-            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
-            present(messageComposeVC, animated: true, completion: nil)
-        } else {
-            let errorAlert = UIAlertController(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", preferredStyle: .alert)
-            errorAlert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
-            self.present(errorAlert, animated: true){}
-        }
     }
 }
--- makeupselector/SelectShadeViewController.swift
+++ makeupselector/SelectShadeViewController.swift
@@ -9,19 +9,9 @@
 import UIKit
 
 class SelectShadeViewController: UIViewController {
-    
-    let messageComposer = MessageComposer()
     var productButtonNumber: Int = Int()
     var shadeButton:Int = Int()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        buttonRed.layer.cornerRadius = 15
-        buttonPink.layer.cornerRadius = 15
-        buttonBerry.layer.cornerRadius = 15
-        buttonCoral.layer.cornerRadius = 15
-        buttonNude.layer.cornerRadius = 15
-    }
+
     @IBOutlet weak var buttonRed: UIButton!
     @IBOutlet weak var buttonPink: UIButton!
     @IBOutlet weak var buttonBerry: UIButton!
@@ -53,18 +43,6 @@ class SelectShadeViewController: UIViewController {
             if let destinationVC = segue.destination as? ShadeViewController{
                 destinationVC.shadeButtonNumber = self.shadeButton
             }
-        }
-    }
-    @IBAction func contactUsButtonClicked(_ sender: UIButton) {
-        if (messageComposer.canSendText()) {
-            
-            // Obtain a configured MFMessageComposeViewController
-            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
-            present(messageComposeVC, animated: true, completion: nil)
-        } else {
-            let errorAlert = UIAlertController(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", preferredStyle: .alert)
-            errorAlert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
-            self.present(errorAlert, animated: true){}
         }
     }
 }
